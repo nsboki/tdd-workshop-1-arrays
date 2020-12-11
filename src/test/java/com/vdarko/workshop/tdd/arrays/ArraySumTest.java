e0f513c325cc14db96848a9df0470a7a30551e29
Bojan Đorđević <nsboki@gmail.com>
2020-12-11 17:24:15 +0100

    Update ArraySumTest.java

--- src/test/java/com/vdarko/workshop/tdd/arrays/ArraySumTest.java
+++ src/test/java/com/vdarko/workshop/tdd/arrays/ArraySumTest.java
@@ -5,20 +5,23 @@ import org.junit.jupiter.api.Test;
 public class ArraySumTest {
 
   /**
-   * <b>GIVEN</b> array of integer values</br>
+   * <b>GIVEN</b> array of integer values {1, 2, 3, 4, 5}</br>
    * <b>WHEN</b> sum is calculated</br>
-   * <b>THEN</b> sum value is as expected</br>
+   * <b>THEN</b> sum value is 15</br>
    */
   @Test
   public void testSum() {
 
     // GIVEN
-    //numbers
+    Integer[] numbers = new Integer[] { 1, 2, 3, 4, 5 };
+    Integer expectedValue = 15;
 
     // WHEN
-    // sum is calculated
+    ArraySum arraySum = new ArraySum();
+    Integer result = arraySum.sum(numbers);
 
     // THEN
-    // assert sum that it is equal to the expected value
+
+
   }
 }
